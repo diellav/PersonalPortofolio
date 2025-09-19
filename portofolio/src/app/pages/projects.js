709f17901efe6b398f33f4b0fc3d6e5eaa871caf
Diella <diellaveliu@gmail.com>
2025-09-19 10:25:38 -0700

    font fix

--- portofolio/src/app/pages/projects.js
+++ portofolio/src/app/pages/projects.js
@@ -105,7 +105,7 @@ export default function Projects() {
             <div className={`bg-violet-400/20 backdrop-blur-md shadow-md sm:w-[95%] -mt-4 w-[100%] md:w-[50%] md:mt-0 lg:mt-0  rounded-xl  overflow-hidden max-[330px]:p-2 p-4 sm:p-4 md:p-4 lg:p-4 relative z-30 ${
       project.id % 2 === 0 ? "sm:-mt-4 md:-mr-16 lg:-mr-48" : "sm:-mt-4 md:-ml-16 lg:-ml-48"}`}>
               <h2 className="text-xl font-semibold mb-2 text-center font-futuristic text-white text-shadow-[0_0_10px_rgba(0,120,255,0.5)]">{project.title}</h2>
-              <p className="text-white lg:text-sm sm:text-xs text-xs font-mono text-center leading-relaxed">{project.description}</p>
+              <p className="text-white lg:text-sm sm:text-base text-base font-mono text-center leading-relaxed">{project.description}</p>
               <div className="flex flex-wrap gap-2 mt-3 mb-3 justify-center">
             {project.techStack?.map((tech, idx) => (
               <span
